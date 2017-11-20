b3ad3ceb1fe31d18b0e302a79b032ea98b6333a5
Adam Kalimi <adam.kalimi@yahoo.com>
2017-11-20 16:17:42 +0800

    Created booking and review models

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120073838) do
+ActiveRecord::Schema.define(version: 20171120081022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "gear_id"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["gear_id"], name: "index_bookings_on_gear_id", using: :btree
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+  end
+
   create_table "gears", force: :cascade do |t|
     t.string   "category"
-    t.string   "gear_name"
-    t.string   "user_id"
+    t.string   "name"
+    t.integer  "user_id"
     t.text     "description"
     t.string   "size"
     t.string   "address"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_gears_on_user_id", using: :btree
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,4 +65,8 @@ ActiveRecord::Schema.define(version: 20171120073838) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "bookings", "gears"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "gears", "users"
+  add_foreign_key "reviews", "bookings"
 end
