253ec2d8b0b8c5354d045c5473f2bcb29a711913
Adam Joseph Kalimi <adam.kalimi@yahoo.com>
2017-11-21 21:11:43 +0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :bookings, only: [:create, :show, :destroy, :update] do
     resources :reviews, only: [:create]
   end
-
+# How does this routing play in with the front end, 
+# considering we want to have the review appear on 
+# the "gears" show page? Waiting to hear back from Sergio on this.
+  
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
