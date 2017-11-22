4a8a0f6a4d9317ed9203692ae6210eb89a4fde10
Forrest Cranmer <letosleepingdragon@gmail.com>
2017-11-22 10:48:14 +0800

    still untested

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   resources :bookings, only: [:create, :show, :destroy, :update] do
     resources :reviews, only: [:create]
   end
-# How does this routing play in with the front end, 
-# considering we want to have the review appear on 
+# How does this routing play in with the front end,
+# considering we want to have the review appear on
 # the "gears" show page? Waiting to hear back from Sergio on this.
-  
+
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
 
-  root to: 'pages#home'
+  root to: 'gears#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :gears
 
