e3f45456c4ac7333d87f3c381e2fc73605449fea
Adam Kalimi <adam.kalimi@yahoo.com>
2017-11-23 16:32:42 +0800

    adding book confirm btn

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,3 +7,27 @@ class PagesController < ApplicationController
     @user = current_user
   end
 end
+
+
+# bookings = current_user.gears.map{|g| g.bookings}.flatten
+
+# bookings.each do |booking|
+
+# if booking.active.nil, show this message
+# "User has reuqested to borrow you #{gear_id}"
+
+# 2 simple form with
+# simple_form_for(@booking)
+# f.hidden_field :active, value: "true"
+# f.submit <confirm>
+
+# 2 simple form with
+# simple_form_for(@booking)
+# f.hidden_field :active, value: "false"
+# f.submit <deny>
+
+# else
+# next
+# end
+
+# end
