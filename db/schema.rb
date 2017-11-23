96b71be37cc731970799d1217abb5a9cacdbf00a
Adam Kalimi <adam.kalimi@yahoo.com>
2017-11-23 14:41:48 +0800

    added ACTIVE column to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171123025253) do
+ActiveRecord::Schema.define(version: 20171123064024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20171123025253) do
     t.datetime "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "active"
     t.index ["gear_id"], name: "index_bookings_on_gear_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
