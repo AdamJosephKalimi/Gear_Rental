1b9eb77b7f0be455f6a1d0a832f056da8fdca211
Adam Kalimi <adam.kalimi@yahoo.com>
2017-11-23 13:46:47 +0800

    completed booking response emails

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -8,8 +8,7 @@ class BookingMailer < ApplicationMailer
   def pending(user)
     @user = user
 
-    mail to: @user.email, subject: "Thank you for your booking #{@user.email}. Awaiting
-    confirmation from this renter", from: "no.reply@Gears.com"
+    mail to: @user.email, subject: "Thank you for your booking!", from: "no.reply@Gears.com"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -21,7 +20,6 @@ class BookingMailer < ApplicationMailer
     @booking = booking
     @renter = booking.gear.user
 
-    mail to: @renter.email, subject: "Congratulations #{@booking.gear.user.email},
-    someone wants to rent your gear! Click #{here} to confirm.", from: "no.reply@Gears.com"
+    mail to: @renter.email, subject: "Confirmation Awaiting!", from: "no.reply@Gears.com"
   end
 end
